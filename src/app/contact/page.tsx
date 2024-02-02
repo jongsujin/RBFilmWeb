cdc0fd7033f0d2daa514c0eec6b8e5918aa0fd30
jinjongsu <jjs9836@naver.com>
2024-02-02 19:38:36 +0900

    Feat: 견적 페이지 폼 완료

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,29 +1,114 @@
 import React from "react";
+import Image from "next/image";
 import Banner from "@/components/Banner/Banner";
 import NavBar from "@/components/NavBar/NavBar";
 import Title from "@/components/Title/Title";
+import Essential from "@/app/contact/_component/essential.svg";
 
 function Estimate() {
   return (
-    <div className="border h-screen">
+    <div className="border border-[#FFCC00]">
       <div className="mb-[600px]">
         <Banner />
       </div>
       <NavBar tab="CONTACT" />
-      <div className="mt-28">
+      <div className="mt-28 mb-48">
         <Title />
       </div>
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-col">
-          <h1>텍스트</h1>
+      <div className="border flex flex-row justify-between">
+        <div className="border w-1/2">
+          <p className="text-[30px] font-bold mb-[60px]">
+            궁금하신 점이 있으신가요?
+          </p>
+          <p className="mb-7 text-headline1">
+            R.B.FILM은 어떤 종류의 프로젝트라도
+            <br />
+            처음부터 끝까지 내 영상을 제작한다는 마인드로 함께합니다.
+          </p>
+          <p className="text-headline1">
+            한 번의 비즈니스가 아닌 지속적인 파트너로 함께 성장하기를 바랍니다.
+          </p>
+          <p className="text-headline1">좋은 인연이 되었으면 합니다.</p>
           <div>
-            <h1>폼</h1>
+            <form className="border mt-[103px]">
+              <div>
+                <div className="flex flex-row gap-1">
+                  <p className="text-headline1 font-bold">
+                    업체(단체명) or 성함
+                  </p>
+                  <Essential className="mt-1" />
+                </div>
+                <input
+                  className="w-[593px] h-11 border border-white bg-black"
+                  type="text"
+                />
+              </div>
+              <div>
+                <div className="flex flex-row gap-1">
+                  <p className="text-headline1 font-bold">이메일</p>
+                  <Essential className="mt-1" />
+                </div>
+                <input
+                  className="w-[593px] h-11 border border-white bg-black"
+                  type="email"
+                />
+              </div>
+              <div>
+                <div className="flex flex-row gap-1">
+                  <p className="text-headline1 font-bold">연락처</p>
+                  <Essential className="mt-1" />
+                </div>
+                <input
+                  className="w-[593px] h-11 border border-white bg-black"
+                  type="text"
+                />
+              </div>
+              <div>
+                <div className="flex flex-row gap-1">
+                  <p className="text-headline1 font-bold">제작 예산</p>
+                  <Essential className="mt-1" />
+                </div>
+                <p className="text-gray font-medium text-[15px]">
+                  정해진 예산이 없다면 &quot;없음&quot;으로 기재해 주세요.
+                </p>
+                <input
+                  className="w-[593px] h-11 border border-white bg-black"
+                  type="text"
+                />
+              </div>
+              <div>
+                <p className="text-headline1 font-bold">레퍼런스 링크</p>
+                <p className="text-gray font-medium text-[15px]">
+                  참고할 수 있는 영상 링크 or 타이틀을 알려주세요. 파일로 가지고
+                  계신 경우 상단 메일로 보내주세요.
+                </p>
+                <input
+                  className="w-[593px] h-11 border border-white bg-black"
+                  type="text"
+                />
+              </div>
+              <div>
+                <div className="flex flex-row gap-1">
+                  <p className="text-headline1 font-bold">문의 사항</p>
+                  <Essential className="mt-1" />
+                </div>
+                <textarea className="w-[593px] h-[203px] border border-white bg-black" />
+              </div>
+              <button className="border" type="button">
+                등록하기
+              </button>
+            </form>
           </div>
         </div>
         <div>
           <h1>사진1</h1>
           <h1>텍스트</h1>
-          <h1>사진2</h1>
+          <Image
+            src="/assets/images/contactPhoto2.png"
+            width={500}
+            height={500}
+            alt="견적문의사진2"
+          />
         </div>
       </div>
     </div>
