bc7ac079610d3fb434080e19a983af0a9e866949
jinjongsu <jjs9836@naver.com>
2024-02-07 22:17:16 +0900

    Feat: 파이어베이스 환경변수화

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAWhnjcq4vA6KClwKYsfFCf9XzJKjJ0TZc",
-  authDomain: "rbfilm-6002a.firebaseapp.com",
-  projectId: "rbfilm-6002a",
-  storageBucket: "rbfilm-6002a.appspot.com",
-  messagingSenderId: "592710761547",
-  appId: "1:592710761547:web:7219eecc182a34172961ce",
-  measurementId: "G-2XYXD9XRPE",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGIN_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
