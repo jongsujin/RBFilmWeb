8399e97dbbe364df10572f0159c0480328ead7ef
jinjongsu <jjs9836@naver.com>
2024-02-12 00:05:18 +0900

    Feat: 브란치 생성 전 커밋

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Banner from "@/components/Banner/Banner";
+import NavBar from "@/components/NavBar/NavBar";
+import Title from "@/components/Title/Title";
 
 function PortFolio() {
   return (
     <div>
-      <h1 className="text-white">포트폴리오</h1>
+      <div className="relative mt-20">
+        <Banner bannerImage="service0.png" />
+      </div>
+      <div className="absolute bottom-[-15.5rem] right-0 left-0">
+        <NavBar tab="PORTFOLIO" />
+        <div className="mt-28 mb-48">
+          <Title title="PORTFOLIO" content="포트폴리오" />
+        </div>
+      </div>
     </div>
   );
 }
